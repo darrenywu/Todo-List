4f49828121b8a2ea0e727cf47573fd0bb7ff08e4
gtabc <darren.wumail15@gmail.com>
2020-05-11 14:06:09 -0700

    Updates

---
+++ precache-manifest.e90e40c5343c52ff4c78da286d3f01db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86447f5231c99a547a6477005717a34",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "ecc3f2b69b29949d979d",
+    "url": "/todo-list/static/css/main.6bd25749.chunk.css"
+  },
+  {
+    "revision": "5625464060a8b3b3ddc1",
+    "url": "/todo-list/static/js/2.557b35bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.557b35bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc3f2b69b29949d979d",
+    "url": "/todo-list/static/js/main.1651fc96.chunk.js"
+  },
+  {
+    "revision": "2d2780fd5dee1f14eb3e",
+    "url": "/todo-list/static/js/runtime-main.5b344d18.js"
+  }
+]);
